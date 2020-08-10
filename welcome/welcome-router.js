0d4d63fe09e2cc7c9c5cd8cc53468066d5257712
Jose Paquian <alostnight1993@outlook.com>
2020-08-10 17:33:37 -0500

    done

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -3,7 +3,7 @@ const welcome = express.Router()
 
 welcome.get("/", (req, res) => {
 	res.json({
-		message: "Welcome to our API",
+		message: "Welcome to our API of Lord of The Rings quotes",
 	})
 })
 
